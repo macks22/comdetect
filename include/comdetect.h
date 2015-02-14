5d990923e8a352ac6f5253231e1ac7508ce4c8ac
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-02-13 21:58:16 -0500

    added new function prototypes and an intArray struct

--- include/comdetect.h
+++ include/comdetect.h
@@ -4,12 +4,13 @@
 /*                            INCLUDES                               */
 /********************************************************************/
 
-#include<stddef.h>
-#include<stdlib.h>
-#include<math.h>
-#include<sys/time.h>
-#include<stdio.h>
-#include<string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 /********************************************************************/
 /*                            STRUCTS                               */
@@ -34,9 +35,19 @@ typedef struct{
 
 } crsMatrix;
 
+typedef struct {
+
+    int length;
+    int data[1];  // space only for the base pointer
+
+} intArray;
+
 /********************************************************************/
 /*                       FUNCTION PROTOTYPES                        */
 /********************************************************************/
+
+// Allocate a new int_array struct
+intArray * newIntArray(int length);
 
 // frees all 3 CRS arrays
 void freeCRSMatrix (crsMatrix *mat);
@@ -50,3 +61,9 @@ void printCRSMatrix(crsMatrix *crs_mat, int storeIDAry);
 // added functionality to decrease computational lookup times by saving out the
 // id array to a file and performing translation in post processing
 void storeAndFreeIDAry(ctrlType *ctrl, crsMatrix *crs_mat);
+
+// Get the degree of a node
+int degree(crsMatrix *graph, int node);
+
+// Breadth-First Search (BFS) implementation
+int bfs(int src, int target);
