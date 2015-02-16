b9fd1c6fda10a2369654e9217351cb5b7f4fa898
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-02-16 09:38:54 -0500

    cleaned up old src files

--- src/graph.c
+++ src/graph.c
@@ -1,75 +1,121 @@
-#include "comdetect.h"
-#include "queue.h"
+#include "graph.h"
 
 
-// Get array of node ids which are neighbors of this node.
-int *neighbors(crsMatrix *graph, int node) {
-    assert(node >= 0);
-    assert(node < graph->num_nodes);
+// Compress edges from edge list into a compressed row storage (CRS) format
+void 
+rowCompressEdges(EdgeList *elist_i, SparseUGraph *graph)
+{
+    EdgeList elist_j;
+    int edge_idx=0, id_idx=0, index_idx=0;
+    int cur_id, prev_id = -1;
+    int i_end, j_end, i, j, i_idx=0, j_idx=0;
 
-    intArray *neighbors;
-    int edge_idx=0, i=0;
-    int start, size;
+    // Allocate space for edge and node storage.
+    // Note that the `mapNodeIds` function allocates space for the id array.
+    graph->index = (int *)tcalloc(graph->n+1, sizeof(int));
+    graph->edges = (int *)tcalloc(graph->m*2, sizeof(int));
 
-    start = graph->index[node];
-    size = graph->index[node+1] - start;
-    neighbors = newIntArray(size);
+    // Now copy the edgelist and sort the copy on the j column
+    copyEdgeList(elist_i, &elist_j);
+    sortEdges(&elist_j, JCOL);
 
-    for (edge_idx=graph->index[node]; edge_idx < graph->index[node+1]; edge_idx++) {
-        neighbors->data[i++] = graph->edges[edge_idx];
+    // For each node in the id array, move through the 2 edge
+    // lists to build up the index and edges arrays.
+    cur_id = graph->id[id_idx];
+    while (id_idx <= graph->n+1) {
+        if (prev_id != cur_id) {
+            // move up in index array
+            graph->index[index_idx++] = edge_idx;
+            prev_id = cur_id;
+        }
+        i = elist_i->nodes[ICOL][i_idx];
+        j = elist_j.nodes[JCOL][j_idx];
+        if (i == cur_id) {
+            i_end = elist_i->nodes[JCOL][i_idx];
+            if (j == cur_id) {
+                // add whichever is smaller
+                j_end = elist_j.nodes[ICOL][j_idx];
+                if (i_end < j_end) {
+                    graph->edges[edge_idx++] = i_end;
+                    i_idx++;
+                } else {
+                    graph->edges[edge_idx++] = j_end;
+                    j_idx++;
+                }
+            } else {
+                // add i value
+                graph->edges[edge_idx++] = i_end;
+                i_idx++;
+            }
+        } else if (j == cur_id) {  // add j value
+            graph->edges[edge_idx++] = elist_j.nodes[ICOL][j_idx++];
+        } else {  // done with this node
+            cur_id = graph->id[id_idx++];
+        }
     }
-    return neighbors;
+
+    // debug
+    assert(id_idx-1 == graph->n+1);
+    printf("graph index:\n");
+    printArray(graph->index, graph->n+1);
+    printf("graph edgelist:\n");
+    printArray(graph->edges, graph->m*2);
 }
 
-// Breadth-First Search (BFS) implementation
-int bfs(crsMatrix *graph, int src, int target) {
-    Queue *q;
-    int i, j, node, neighbor, cur_layer;
-    int visited[graph->num_nodes];
-    int parent[graph->num_nodes];
-    int distance[graph->num_nodes];
+void
+readSparseUGraph(InputArgs *args, SparseUGraph *graph)
+{
+    FILE *fpin;
+    EdgeList elist;
+    int num_ids, edge_idx=0;
 
-    // TODO: perhaps store these 3 arrays in a bfs_data struct?
-    // then post-processing could be used to determine shortest paths.
+    // read the first line, which contains #nodes #edges
+    fpin = fopen(args->infile, "r");
+    fscanf(fpin, "%d %d", &graph->n, &graph->m);
+    printf("reading: %d nodes, %d edges\n", graph->n, graph->m);
 
-    // initialize tracking data
-    for (i=0; i < graph->num_nodes; i++) {
-        visited[i] = 0;
-        parent[i] = i;
-        distance[i] = 0;
+    // allocate a new edgelist to work with while converting to CRS
+    newEdgeList(&elist, graph->m);
+
+    // now read in the edgelist, one at a time
+    while (!feof(fpin)) {
+        fscanf(fpin, "%d %d",
+               &elist.nodes[ICOL][edge_idx],
+               &elist.nodes[JCOL][edge_idx]);
+        edge_idx++;
+    }
+    printf("# edges read: %d\n", --edge_idx);
+    printEdgeList(&elist, edge_idx);
+    assert(graph->m == edge_idx);
+
+    // get listing of all unique node ids
+    mapNodeIds(&elist, &graph->id, &num_ids);
+    printArray(graph->id, num_ids);
+    assert(graph->n == num_ids);
+
+    // compress edgelist rows to construct index and edge list
+    rowCompressEdges(&elist, graph);
+
+    // Write out the ID array; we won't be using it while processing.
+    // It can be used later to translate the output (in node indices)
+    // to the input node IDs.
+    // storeAndFreeIDArray(graph);
+    fclose(fpin);
+}
+
+void
+storeAndFreeNodeIds(SparseUGraph *graph)
+{
+    int i;
+    FILE *fpout;
+    char store_file[50] = "../output_and_stored_id_ary/storedIDAry.txt";
+
+    fpout = fopen(store_file, "w");
+    for (i=0; i < graph->n; i++) {
+        fprintf(fpout, "%d\n", graph->id[i]);
     }
 
-    q = newQueue();
-    enqueue(q, src);
-    visited[src] = 1;
-    distance[src] = 0;
-    parent[src] = src;  // root node
-
-    while (!queueIsEmpty(q)) {
-        // check if next node is our search target
-        node = dequeue(q);
-        if (node == target) { // search finished
-            return distance[node];
-        }
-
-        // visit all immediate neighbors
-        cur_layer = distance[node] + 1;
-        for (i=graph->index[node]; i < graph->index[node+1]; i++) {
-            neighbor = graph->edges[i];
-
-            if (!visited[neighbor]) {
-                parent[neighbor] = node;
-                distance[neighbor] = cur_layer;
-                visited[neighbor] = 1;
-
-                // add all of this neighbors' neighbors to the queue
-                for (j=graph->index[neighbor]; i < graph->index[neighbor+1]; i++) {
-                    enqueue(q, neighbor);
-                }
-            }
-        }
-    }
-
-    // return -1 to indicate the search failed; target unreachable
-    return -1;
+    fclose(fpout);
+    free(graph->id);
+    printf("Node id array stored in %s\n", store_file);
 }
