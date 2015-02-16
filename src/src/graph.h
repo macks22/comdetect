e6d81c71e011c7208d9c666143acbfff495613b6
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-02-16 09:22:35 -0500

    reorganize header file; update old defs

--- src/src/graph.h
+++ src/src/graph.h
@@ -15,11 +15,31 @@
 /*                       STRUCTS AND PROTOTYPES                     */
 /********************************************************************/
 
+///////////////////////////////////////
+// UTILITY FUNCTIONS
+
 // print error and exit
 void error(int err_code);
 
 // attempt to calloc memory, error out if failure
 void *tcalloc(size_t nitems, size_t size);
+
+// find the largest number in the array
+int findLargest(int *array, int length);
+
+// perform an in-place radix sort on the array; result is ascending order
+void radixSort(int *array, int length);
+
+// remove all duplicate values from the integer array
+// return the size of the new array
+void removeDuplicates(int *array, int *length);
+
+// print an array of integers
+void printArray(int *array, int length);
+
+
+///////////////////////////////////////
+// EDGE HANDLING
 
 // struct to hold user input arguments
 typedef struct {
@@ -28,8 +48,9 @@ typedef struct {
     int num_clusters;
     char outfile[200];
 
-} inputArgs;
+} InputArgs;
 
+// struct to hold edgelist when converting file to CRS graph
 typedef struct {
 
     int *nodes[2];  // start and end nodes
@@ -39,6 +60,9 @@ typedef struct {
 
 #define ICOL    0
 #define JCOL    1
+
+#define IEND (elist, idx) (elist->nodes[ICOL][idx])
+#define JEND (elist, idx) (elist->nodes[JCOL][idx])
 
 
 // allocate space for a new edge list of the given length
@@ -56,19 +80,6 @@ void sortEdges(EdgeList *elist, int column);
 // find largest value in i or j column
 int findLargestEndpoint(EdgeList *elist, int column);
 
-// find the largest number in the array
-int findLargest(int *array, int length);
-
-// perform an in-place radix sort on the array; result is ascending order
-void radixSort(int *array, int length);
-
-// remove all duplicate values from the integer array
-// return the size of the new array
-void removeDuplicates(int *array, int *length);
-
-// print an array of integers
-void printArray(int *array, int length);
-
 // Return an array with all unique node ids sorted in ascending order.
 void mapNodeIds(EdgeList *elist, int **idmap, int *num_nodes);
 
@@ -76,28 +87,37 @@ void mapNodeIds(EdgeList *elist, int **idmap, int *num_nodes);
 void printEdgeList(EdgeList *elist, int num_edges);
 
 
+///////////////////////////////////////
+// GRAPH FUNCTIONS
+
 // Sparse undirected graph.
-// Utilizes a non-symmetric CSR format for compression
-// In other words, maintain a duplicate copy of each edge
-// in order to facilitate quick lookup of neighbors.
+// Utilizes a non-symmetric CSR format for compression In other words,
+// maintain a duplicate copy of each edge in order to facilitate quick
+// lookup of neighbors.
+//
+// Since an undirected graph has symmetric edges, we normally only store
+// each once. However, storing each edge as (i, j) and (j, i) allows
+// faster lookup of neighbors. If we stored each edge once, and we ask
+// who are the neighbors of i, we must look at i's edgelist, and also
+// every other node's edgelist to see if i is present in it. If we
+// duplicate the edges, then we must only check i's edgelist.
 typedef struct {
 
     int n;          // number of nodes: |V|
     int m;          // number of edges: |E|
 
     int *id;        // size = |V|; ids for all nodes
-
-    int *index_ij;  // size = |V| + 1
-    int *edges_ij;  // size = |E|
-
-    int *index_ji;  // size = |V| + 1
-    int *edges_ji;  // size = |E|
+    int *index;     // size = |V| + 1
+    int *edges;     // size = 2|E|
 
 } SparseUGraph;
 
 
+// Compress edges from edge list into a compressed row storage (CRS) format
+void rowCompressEdges(EdgeList *elist, SparseUGraph *graph);
+
 // read a sparse undirected graph from an edgelist file
-void readSparseUGraph(inputArgs *args, SparseUGraph *graph);
+void readSparseUGraph(InputArgs *args, SparseUGraph *graph);
 
 // free the sparse undirected graph
 void freeSparseUGraph(SparseUGraph *graph);
@@ -132,5 +152,5 @@ typedef struct {
 
 // Perform a BFS on the sparse undirected graph and return
 // the information discovered.
-BFSInfo * bfs(SparseUGraph *graph);
+void bfs(SparseUGraph *graph, BFSInfo *info);
 
